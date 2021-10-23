be47218ac46c20d818c2650a7a98c70234abb51a
BongumusaSizwe <bongumusasizwemavuso@gmail.com>
2021-10-23 19:13:34 +0200

    New CNN model

--- cnn/atari_train_cnn.py
+++ cnn/atari_train_cnn.py
@@ -4,108 +4,193 @@ import gym
 import numpy as np
 from scipy import stats
 import torch
+import torchvision
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 import torch.optim as optim
+import os
+import json
+from argparse import ArgumentParser
+import argparse
 
+def parse_args():
+    parser = argparse.ArgumentParser("CNN experiments for Atari games")
+    parser.add_argument("--seed", type=int, default=42, help="which seed to use")
+    # Environment
+    parser.add_argument("--env", type=str, default="PongNoFrameskip-v4", help="name of the game")
+    # Core CNN parameters
+    parser.add_argument("--optimizer", type=str, default="Adam", 
+    					help="Name of the optimizer to be used\n Available Optimizers \n 1. Adam\n2. SGD")
+    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate for Adam or SGD optimizer")
+    parser.add_argument("--num-steps", type=int, default=int(1e6),
+                        help="total number of steps to run the environment for")
+    parser.add_argument("--batch-size", type=int, default=32, help="number of transitions to optimize at the same time")
+    parser.add_argument("--num-epochs", type=int, default=2, help="number of epochs")
+    parser.add_argument("--print-freq", type=int, default=10, help="print frequency.")
+    parser.add_argument("--num-workers", type=int, default=2, help="number of workers in the network")
+    parser.add_argument("--num-channels", type=int, default=4, help="number of input channels")
+    parser.add_argument("--num-classes", type=int, default=6, help="number of output classes")
+    return parser.parse_args()
 
-#Preprocessor(possibly inefficient)
-def preprocess(states, actions):
-    """Preprocess training data, saved as p"""
-    states2 = []
-    # Leave necessary details only
-    for image in states:
-        # states2.append(image[125:190, 10:150])
-        states2.append(image[:, :])
-    l = len(states2)
+# Save file
+def path_name(env_name):
+	directory = 'Models/'
+	#env_name = env.unwrapped.spec.id.lower()
+	env_name = env_name.lower()
+	path = os.path.join(directory, env_name)
+	dir_exist = os.path.isdir(path)
+	dataset_name = env_name
 
-    while l % 3 != 0:
-        l = l-1
-    states = np.array(states2)
-    acts = []
-    # Stack the states in succession of 3 states pair image
-    new_states = []
-    for i in range(0, l, 3):
-        rgb = np.dstack((states[i], states[i+1], states[i+2]))
-        new_states.append(rgb)
-        tmp_arr = actions[i:i+3]
-        a = stats.mode(tmp_arr)
-        a = a[0][0]
-        acts.append(a)
-    actions = np.array(acts)
-    actions = torch.from_numpy(actions)
-    states = np.array(new_states)
+	if not dir_exist:
+		os.mkdir(path)
 
-    states = torch.from_numpy(states).float()
+	count = 1
+	for dirname, dirnames, filenames in os.walk(path):
+		for filename in filenames:
+		    print(os.path.join(dirname, filename))
+		    count += 1
+	#Create and save dataset
+	loc = dataset_name.find('-')
+	filepath = dataset_name[:loc]+str(count)
+	
+	return directory + env_name +'/' +  filepath
 
-    return states, actions
 
 class GameData(Dataset):
-    def __init__(self, name = 'RoadRunner.npz'):
-        game = np.load(name)
-        self.states = game['states']
-        self.actions = game['actions']
-        self.n_samples = self.actions.shape[0]
-        print('Starting preprocess')
-        self.states, self.actions = preprocess(self.states, self.actions)
-        l, w, h, c = self.states.shape
-        self.states = self.states.reshape([l, c, w, h])
-        print('Initialization finished......')
-        
-    def __len__(self):
-        return self.n_samples
+	def __init__(self, name = 'pongnoframeskip1.npz'):
+		##Load dataset
+		game = np.load(name)
+		game2 = np.load('pongnoframeskip2.npz')
+		states1 = game['states'].astype(np.float32)
+		states2 = game2['states'].astype(np.float32)
+		print(states1.shape, states2.shape)
+		self.states = np.append(states1, states2, axis = 0)
+		print(self.states.shape)
+		act1 = game['actions']
+		act2 = game2['actions']
+		self.actions = np.append(act1, act2)
+		
+	#        self.states = game['states'].astype(np.float32)
+	#        self.actions = game['actions']
+		
+		self.n_samples = self.actions.shape[0]
+		self.actions = torch.from_numpy(self.actions)
+		self.states = torch.from_numpy(self.states)
+		
+	def __len__(self):
+		return self.n_samples
+
+	def __getitem__(self, index):
+		return self.states[index], self.actions[index]
     
-    def __getitem__(self, index):
-        return self.states[index], self.actions[index]
 
 if __name__ == '__main__':
-    dataset = GameData()
+	
+	args = parse_args()
 
-    dataloader = DataLoader(dataset= dataset, batch_size = 3, shuffle = True, num_workers = 2)
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    batch_size = 4
+	batch_size = args.batch_size
+	num_workers = args.num_workers
+	num_channels = args.num_channels
+	num_classes = args.num_classes
+	learning_rate = args.lr
+	epochs = args.num_epochs
+	opt_name = args.optimizer
+	print_freq = args.print_freq
+	
+	# dataloader = DataLoader(dataset= trainset, batch_size = batch_size, shuffle = True, num_workers = num_workers)
 
-    trainset = dataset
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,num_workers=2)
+#	trainset = GameData()
+	dataset = GameData()
+	
+	transform = transforms.Compose(
+		[transforms.ToTensor(),
+		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    classes = tuple(np.arange(0, 10))
+#	train_size = int(0.8*len(dataset))
+#	traindata, testdata = torch.utils.data.random_split(dataset, [train_size, test_size])
 
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
+	train_size = int(0.8* len(dataset))
+	test_size = len(dataset) - train_size
+	trainset, testset = torch.utils.data.random_split(dataset, [train_size, test_size])
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,num_workers= num_workers)
 
-    net  = CNN(3, 10)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    print('Training starting................................')
-    for epoch in range(2):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-            if i == 71:
-                break
-            # zero the parameter gradients
-            optimizer.zero_grad()
+	# Number of classes can be found using env.actions.n from gym environment
+	classes = tuple(np.arange(0, num_classes))
 
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            
-            # print statistics
-            running_loss += loss.item()
-            if i % 10 == 0:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 10))
-                running_loss = 0.0
+	dataiter = iter(trainloader)
+	images, labels = dataiter.next()
 
-    print('Finished Training')
+	net  = CNN(num_channels, num_classes) 
+	criterion = nn.CrossEntropyLoss()
+	if opt_name == "SGD":
+		optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+	else:
+		optimizer = optim.Adam(net.parameters(), lr=learning_rate)
 
+	print('Training starting................................')
+	for epoch in range(epochs):  # loop over the dataset multiple times
+		running_loss = 0.0
+		for i, data in enumerate(trainloader, 0):
+		    # get the inputs; data is a list of [inputs, labels]
+		    inputs, labels = data
+
+		    # zero the parameter gradients
+		    optimizer.zero_grad()
+
+		    # forward + backward + optimize
+		    outputs = net(inputs)
+		    loss = criterion(outputs, labels)
+		    loss.backward()
+		    optimizer.step()
+		    
+		    # print statistics
+		    running_loss += loss.item()
+		    if i % print_freq == 0:    # print every print_freq mini-batches
+		        print('[%d, %5d] loss: %.3f' %
+		            (epoch + 1, i + 1, running_loss / print_freq))
+		        running_loss = 0.0
+
+	print('Finished Training')
+
+	PATH = path_name(args.env)
+	torch.save(net.state_dict(), PATH + '.npz')
+	
+	# Test Network Performance
+#	testset = GameData(name='pongnoframeskip2.npz')
+	testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,num_workers= num_workers)
+	print('Starting training')
+	correct = 0
+	total = 0
+	
+	with torch.no_grad():
+		for data in testloader:
+			images, labels = data
+			
+			# calculate outputs by running images through the network
+			outputs = net(images)
+			# the class with the highest energy is what we choose as prediction
+			_, predicted = torch.max(outputs.data, 1)
+			total += labels.size(0)
+			correct += (predicted == labels).sum().item()
+	
+	results = 100*(correct/total)
+	print('Accuracy of the network on the test set: %d %%' % (
+    	results))
     
-    PATH = './Training1.pth'
-    torch.save(net.state_dict(), PATH)
+    
+	#Save environement details
+	details ={
+		"env": args.env,
+		"optmizer": args.optimizer,
+		"lr": args.lr,
+		"num-steps": args.num_steps,
+		"num-epochs": args.num_epochs,
+		"num-workers": args.num_workers,
+		"num-channels": args.num_channels,
+		"num-classes": args.num_classes,
+		"test-results": results
+		}
+	with open(PATH+'.json', 'w') as fp:
+		json.dump(details, fp)
--- cnn/cnnmodel.py
+++ cnn/cnnmodel.py
@@ -7,7 +7,14 @@ class CNN(nn.Module):
     """
     Implementation of a Deep Convolutional Neural Network. The architecture is the same as that used in the
     Nature DQN paper
-
+	(channels): Number of channels per input array
+	(action_space): Expected number of output categories
+	(width): Image width
+	(height): Image height
+	(num_layers): Number of layers in the network
+	(stride_li): A list containing strides for each layer
+	(kern_li):  A list containing kernel sizes for each layer
+	================================================================
     (conv1): Conv2d(210, 32, kernel_size=(8, 8), stride=(4, 4))
     (conv2): Conv2d(32, 64, kernel_size=(4, 4), stride=(2, 2))
     (conv3): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1))
@@ -15,10 +22,10 @@ class CNN(nn.Module):
     (linear2): Linear(in_features=512, out_features=6, bias=True)
 
     """
-    def __init__(self, observation_space, action_space, num_layers = 3, stride_li= [8, 2, 2], kern_li = [8, 3, 3]):
+    def __init__(self, channels, action_space, width = 84, height = 84, num_layers = 3, stride_li= [8, 2, 2], kern_li = [8, 3, 3]):
 
         super().__init__()
-        self.conv1 = nn.Conv2d(observation_space, 32, kern_li[0], stride = stride_li[0])
+        self.conv1 = nn.Conv2d(channels, 32, kern_li[0], stride = stride_li[0])
         self.conv2 = nn.Conv2d(32, 64, kern_li[1], stride= stride_li[1])
         self.conv3 = nn.Conv2d(64, 64, kern_li[2], stride = stride_li[2])
         def conv_returns(w, k, s, p = 0):
@@ -31,9 +38,8 @@ class CNN(nn.Module):
             p: pad sie
             '''
             return int((w-k+2*p)/s + 1 )
-        # Input is an image of 210 by 160
-        self.a =   210
-        self.b = 160
+        self.a =   width
+        self.b = height
         for i in range(num_layers - 1):
             self.a = conv_returns(self.a, kern_li[i], stride_li[i])
             self.b = conv_returns(self.b, kern_li[i], stride_li[i])
